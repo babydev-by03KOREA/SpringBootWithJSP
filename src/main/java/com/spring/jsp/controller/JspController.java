341c9bd56270148ece6d965b61a77f00f7628aa7
박형주 <hyeongju1122@gmail.com>
2023-01-30 00:50:05 +0900

    thymeleaf > JSP migrations, @RequestParam

--- src/main/java/com/spring/jsp/controller/JspController.java
+++ src/main/java/com/spring/jsp/controller/JspController.java
@@ -1,10 +1,17 @@
 package com.spring.jsp.controller;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.jsp.dto.ProductDTO;
 
 @Controller
 @RequestMapping(value = "/jsp")
@@ -14,5 +21,40 @@ public class JspController {
 		model.addAttribute("hello", "JSP + SPRING BOOT 폼 미쳤다");
 		mav.setViewName("index");
 		return mav;
+	}
+
+	@GetMapping(value = "/product")
+	public String jspProduct(Model model) {
+		ProductDTO productDTO = new ProductDTO();
+		productDTO.setItemDetail("iphone 14 Pro");
+		productDTO.setItemName("프로 그 이상.");
+		productDTO.setPrice(1550000);
+		productDTO.setRegTime(LocalDateTime.now());
+
+		model.addAttribute("productDTO", productDTO);
+		return "product/product";
+	}
+
+	@GetMapping(value = "/products")
+	public String jspProducts(Model model) {
+		List<ProductDTO> productList = new ArrayList<>();
+		for (int i = 1; i <= 10; i++) {
+			ProductDTO productDTO = new ProductDTO();
+			productDTO.setItemDetail("아이폰 시리즈 " + i);
+			productDTO.setItemName("아이폰 " + i);
+			productDTO.setPrice(100000 * i);
+			productDTO.setRegTime(LocalDateTime.now());
+			productList.add(productDTO);
+		}
+		model.addAttribute("itemList", productList);
+		return "product/products";
+	}
+
+	@GetMapping(value = "/jspUrl")
+	public String jspUrlParameter(@RequestParam("param1") String param1, @RequestParam("param2") String param2,
+			Model model) {
+		model.addAttribute("param1", param1);
+		model.addAttribute("param2", param2);
+		return "link";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jstl/core_rt" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,8 +10,11 @@
 <body>
 	<%
 		/* String hello = "springBoot + JSP"; */
-		String hello = (String)request.getAttribute("hello");
+		String hello = (String) request.getAttribute("hello");
 	%>
 	<h1><%=hello%></h1>
+	<div>
+		<a href="jsp/jspUrl?param1=파라미터 1 JSP&param2=파라미터 2 JSP">파라미터 전달</a>
+	</div>
 </body>
 </html>
